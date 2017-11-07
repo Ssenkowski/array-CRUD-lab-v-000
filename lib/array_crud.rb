0a069316958a9bd6cd05d72bb34d34fa4eadd604
Steven Senkowski <scsenkowski@gmail.com>
2017-11-07 17:43:53 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ end
 
 def add_element_to_start_of_array(array, element)
 ["I", "am", "really", "learning"]
-add_element_to_start_of_array.unshift("wow")
+add_element_to_start_of_array(array, elemet).unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
