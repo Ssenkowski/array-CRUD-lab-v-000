c52be2b0b6c47e3b3cc8bbb97ed169ed65fc3fba
Steven Senkowski <scsenkowski@gmail.com>
2017-11-07 17:40:29 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 def add_element_to_start_of_array(array, element)
   [1,2,3,4]
   add_element_to_start_of_array.unshift("arrays!")
-
+  puts add_element_to_start_of_array.inspect
 end
 
 def remove_element_from_end_of_array(array)
