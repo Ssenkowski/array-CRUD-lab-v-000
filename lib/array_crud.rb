b19e55a7cc65598c753e57861d9ec3aff893d14e
Steven Senkowski <scsenkowski@gmail.com>
2017-11-07 17:48:53 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,15 +27,15 @@ end
 
 def retrieve_element_from_index(array, index_number)
   my_array = ["I", "am", "really", "learning"]
-
+  my_array[1]
 end
 
 def retrieve_first_element_from_array(array)
   my_array = ["I", "am", "really", "learning"]
-
+  my_array[0]
 end
 
 def retrieve_last_element_from_array(array)
   my_array = ["I", "am", "really", "learning"]
-
+  my_array[-1]
 end
