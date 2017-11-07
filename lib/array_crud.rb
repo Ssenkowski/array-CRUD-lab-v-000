c226afc5bdccfa3f24a0d710efe19d8454b89c71
Steven Senkowski <scsenkowski@gmail.com>
2017-11-07 17:52:33 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ end
 
 def remove_element_from_end_of_array(array)
   my_array = ["I", "am", "really", "learning"]
-  my_array.pop
+  learning_element = my_array.pop
+  puts learning_element
 end
 
 def remove_element_from_start_of_array(array)
